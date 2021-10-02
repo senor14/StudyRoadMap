3f6257d7056a990c3a8b64701c1271820b86a4cb
SBKim12 <mar5924@naver.com>
2021-10-02 16:38:33 +0900

    [ADD] 스케쥴 프로그램 이식

---
+++ src/main/webapp/resources/js/calendar/editEvent.js
@@ -0,0 +1,135 @@
+/* ****************
+ *  일정 편집
+ * ************** */
+var editEvent = function (event, element, view) {
+
+    $('#deleteEvent').data('id', event._id); //클릭한 이벤트 ID
+	
+    $('.popover.fade.top').remove();
+    $(element).popover("hide");
+
+    if (event.allDay === true) {
+        editAllDay.prop('checked', true);
+    } else {
+        editAllDay.prop('checked', false);
+    }
+
+    if (event.end === null) {
+        event.end = event.start;
+    }
+
+    if (event.allDay === true && event.end !== event.start) {
+        editEnd.val(moment(event.end).subtract(1, 'days').format('YYYY-MM-DD HH:mm'))
+    } else {
+        editEnd.val(event.end.format('YYYY-MM-DD HH:mm'));
+    }
+
+    modalTitle.html('일정 수정');
+    editTitle.val(event.title);
+    editStart.val(event.start.format('YYYY-MM-DD HH:mm'));
+    editType.val(event.type);
+    editDesc.val(event.description);
+    editColor.val(event.backgroundColor).css('color', event.backgroundColor);
+
+    addBtnContainer.hide();
+    modifyBtnContainer.show();
+    eventModal.modal('show');
+
+    //업데이트 버튼 클릭시
+    $('#updateEvent').unbind();
+    $('#updateEvent').on('click', function () {
+		
+        if (editStart.val() > editEnd.val()) {
+            alert('끝나는 날짜가 앞설 수 없습니다.');
+            return false;
+        }
+
+        if (editTitle.val() === '') {
+            alert('일정명은 필수입니다.')
+            return false;
+        }
+
+        let statusAllDay;
+        let startDate;
+        let endDate;
+        let displayDate;
+		
+		
+        if (editAllDay.is(':checked')) {
+            statusAllDay = true;
+            startDate = moment(editStart.val()).format('YYYY-MM-DD');
+            endDate = moment(editEnd.val()).format('YYYY-MM-DD');
+            displayDate = moment(editEnd.val()).add(1, 'days').format('YYYY-MM-DD');
+        } else {
+            statusAllDay = false;
+            startDate = editStart.val();
+            endDate = editEnd.val();
+            displayDate = endDate;
+        }
+
+        eventModal.modal('hide');
+
+        event.allDay = statusAllDay;
+        event.title = editTitle.val();
+        event.start = startDate;
+        event.end = displayDate;
+        event.type = editType.val();
+        event.backgroundColor = editColor.val();
+        event.description = editDesc.val();
+		
+        $("#calendar").fullCalendar('updateEvent', event);
+		
+		
+		
+        //일정 업데이트
+       $.ajax({
+            type: "get",
+            url: "updateSchedule.do",
+            data: {
+            	"_id" : event._id,
+            	"allDay" : event.allDay,
+            	"title" : event.title ,
+				"start" : event.start ,
+				"end" : event.end  ,
+				"type" : event.type ,
+				"backgroundColor" : event.backgroundColor,
+				"description" : event.description
+            },
+            success: function (response) {
+                if(response==1){
+                	console.log("성공");
+                }else{
+              	  console.log("실패");
+                }
+            }
+        });
+
+    });
+};
+
+// 삭제버튼
+$('#deleteEvent').on('click', function () {
+    
+    $('#deleteEvent').unbind();
+    $("#calendar").fullCalendar('removeEvents', $(this).data('id'));
+    eventModal.modal('hide');
+	
+	let id = $(this).data('id');
+	
+    //삭제시
+    $.ajax({
+        type: "get",
+        url: "delSchedule.do",
+        data: {
+        	"id" :  id
+        },
+        success: function (response) {
+        	if(response==1){
+        		console.log("삭제 되었습니다.");
+        	}else{
+        		console.log("삭제 실패했습니다.");
+            }
+        }
+    });
+
+});
